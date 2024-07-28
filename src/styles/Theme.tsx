30fb9f3768d387006529e3795d3b29eef0f9454e
AtanasovCode <a_ljupce@yahoo.com>
2024-07-28 15:55:28 +0200

    cleaned up theme

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -1,27 +1,9 @@
 const theme = {  
     logoFont: "'Keania One', cursive",
     font: "'Noto Sans', sans-serif",
-  
-    richBlack: "#0f1f2e",
-    darkBlue: "hsl(203, 94%, 12%)",
-    darkPurple: "#740743",
-    lightGreen: "#73e77d",
-    softOrange: "hsl(13, 77%, 52%)",
-  
-    magnolia: "hsl(273, 33%, 94%)",
-    mayaBlue: "#5dbdf9",
-    mayaBluePale: "#5dbdf92b",
+    
     mayaBlueDark: "rgba(93, 189, 249, .3)",
-    lightPurple: "#dd02b8",
-    darkYellow: "#FF950A",
-    aquamarine: "#04F1AA",
-  
-    lighterGreen: "hsl(125, 88%, 79%)",
-    darkGreen: "hsl(125, 100%, 32%)",
     richBlackDark: "#060d14",
-  
-    barFill: "#000",
-
 
     background: "#0f1f2e",
     primary: "#733598",
